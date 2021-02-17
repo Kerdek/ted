9ff42d4fe723d098afcef312109ddf069bd7e241
Ted <kerdek7@gmail.com>
2021-02-17 15:01:15 -0500

    Rename and refactor 'address_of.hpp' to 'pointer.hpp'.

---
+++ include/ted/pointer.hpp
@@ -0,0 +1,58 @@
+/* 
+               pointer.hpp
+
+        -- built-in indirection --
+
+original (c) 2021 theodoric e. stier
+public domain
+
+*/
+
+#ifndef H_68C63DFA_8F3F_4238_AA8B_6BB10A544AFE
+#define H_68C63DFA_8F3F_4238_AA8B_6BB10A544AFE
+
+#include <memory>
+#include <type_traits>
+
+namespace ted
+{
+
+/**
+
+Get the address of an object `x`.
+Effectively an alias of
+`std::addressof`.
+
+*/
+template<
+    typename T>
+    auto address_of(
+        T &x)
+    noexcept -> decltype(
+        std::addressof(x))
+{
+    return std::addressof(x);
+}
+
+/**
+
+Dereference a pointer `x` with no null
+check. Refuses anything which is not
+actually a pointer.
+
+*/
+template<
+    typename T>
+    auto indirect(
+        T &&x)
+    noexcept -> decltype(
+        *x)
+{
+    static_assert(std::is_pointer_v<T>);
+
+    return *x;
+}
+
+}
+
+#endif
