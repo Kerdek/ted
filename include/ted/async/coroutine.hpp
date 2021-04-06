14fe9b7e9e21eca889a0587295b21d053e7d93b5
Ted <kerdek7@gmail.com>
2021-04-06 15:04:45 -0400

    remove suspends concept

--- include/ted/async/coroutine.hpp
+++ include/ted/async/coroutine.hpp
@@ -15,14 +15,13 @@ are generated from types satisfying
 #ifndef H_0493F53E_EFBF_465E_91AD_4A7A12CC8AB0
 #define H_0493F53E_EFBF_465E_91AD_4A7A12CC8AB0
 
-#include <ted/async/suspends.hpp>
 #include <ted/async/promise.hpp>
 
 namespace ted::async
 {
 
 template<
-    suspends Coroutine>
+    typename Coroutine>
     struct coroutine
 {
     using promise_type = promise<
